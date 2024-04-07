42150f22070e7ed2e330dbccd1b0b75ffe462750
Bhanuka Senevirathne <bhanuka76@gmail.com>
2024-04-07 12:31:55 +0530

    Add project files.

---
+++ ArmstrongNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ArmstrongNumbers
+{
+    using System;
+
+    class ArmstrongNumbers
+    {
+        static bool IsArmstrong(int number)
+        {
+            int originalNumber = number;
+            int sum = 0;
+            int numberOfDigits = 0;
+
+            while (number > 0)
+            {
+                number /= 10;
+                numberOfDigits++;
+            }
+
+            number = originalNumber;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += (int)Math.Pow(digit, numberOfDigits);
+                number /= 10;
+            }
+
+            return sum == originalNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Armstrong Numbers (0 to 100,000): ");
+
+            bool firstNumber = true;
+            for (int number = 0; number < 100001; number++)
+            {
+                if (IsArmstrong(number))
+                {
+                    if (firstNumber)
+                    {
+                        firstNumber = false;
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                    Console.Write(number);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
